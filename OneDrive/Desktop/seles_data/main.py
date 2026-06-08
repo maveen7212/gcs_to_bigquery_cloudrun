1a819405fad4e9ad1d711903710d48ee36026f6b
maveen7212 <maveen7212@gmail.com>
2026-06-08 18:28:38 +0530

    cloud build

--- OneDrive/Desktop/seles_data/main.py
+++ OneDrive/Desktop/seles_data/main.py
@@ -9,11 +9,19 @@ def load_data():
     client = bigquery.Client()
 
     uri = "gs://sale-data-bkt/sales.txt"
-    job = client.load_table_from_uri(
-        uri,
-        "project-77158318-bf9d-4d5d-bcc.gcp_project.sales_data"
+
+    job_config = bigquery.LoadJobConfig(
+        source_format=bigquery.SourceFormat.CSV,
+        skip_leading_rows=1,
+        autodetect=True
     )
 
-    job.result()
+    load_job = client.load_table_from_uri(
+        uri,
+        "project-77158318-bf9d-4d5d-bcc.gcp_project.sales_data",
+        job_config=job_config
+    )
+
+    load_job.result()
 
     return "Data Loaded Successfully"
